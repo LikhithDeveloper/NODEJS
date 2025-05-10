a67df28ca9c45c1eafc5de991c986e460ce777e5
Likhith Panigrahi <LikhithDeveloper>
2025-05-10 15:05:19 +0530

    Task-1 Done

---
+++ learning/task1/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render("task1", { files: files });
+  });
+});
+
+app.post("/create", (req, res) => {
+  const { title, details } = req.body;
+  fs.writeFile(`./files/${title.split(" ").join("")}/`, details, (err) => {
+    res.redirect("/");
+  });
+});
+app.listen(port, () => {
+  console.log(`Server is started under port: ${port}`);
+});
