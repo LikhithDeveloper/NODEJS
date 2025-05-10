3124d949677ecda7f7ef45f9fb975dac29b16d35
Likhith Panigrahi <LikhithDeveloper>
2025-05-10 17:04:51 +0530

    Task fully Done

--- learning/task1/server.js
+++ learning/task1/server.js
@@ -22,6 +22,29 @@ app.post("/create", (req, res) => {
     res.redirect("/");
   });
 });
+
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}/`, "utf-8", (err, fileData) => {
+    // console.log(fileData);
+    res.render("show", { fileData: fileData });
+  });
+});
+
+app.get("/edit/:file", (req, res) => {
+  res.render("edit", { file: req.params.file });
+});
+
+app.post("/editfile", (req, res) => {
+  console.log(req.body);
+  fs.rename(
+    `./files/${req.body.prev}/`,
+    `./files/${req.body.new}.txt`,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Server is started under port: ${port}`);
 });
